4ad132095cc5fc60f2966f023108b04410f721f9
dbsduy <duy.tran@deltabrains.vn>
2024-06-07 10:40:02 +0900

    add: loading state for pages

--- Services/AuthApi.cs
+++ Services/AuthApi.cs
@@ -27,17 +27,25 @@ namespace ECommerceApp.Services
         public async Task<IToken> Login(IAuth body)
         {
             var res = await _httpClient.PostAsJsonAsync("/auth/login", body);
-            var resultJson = await res.Content.ReadFromJsonAsync<IToken>();
+            if (res.IsSuccessStatusCode)
+            {
+                var resultJson = await res.Content.ReadFromJsonAsync<IToken>();
 
-            var token = resultJson.Token.Trim('"');
+                var token = resultJson.Token.Trim('"');
 
-            await _localStorage.SetItemAsync("authToken", token);
-            await _localStorage.SetItemAsync("username", token);
+                await _localStorage.SetItemAsync("authToken", token);
+                await _localStorage.SetItemAsync("username", token);
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(body.Username);
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                return resultJson;
 
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            ((ApiAuthenticationStateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(body.Username);
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            return resultJson;
+            }
+            else
+            {
+                await _jSRuntime.InvokeVoidAsync("alert", "Username or password is incorrect!");
+                return new IToken();
+            }
         }
 
 
